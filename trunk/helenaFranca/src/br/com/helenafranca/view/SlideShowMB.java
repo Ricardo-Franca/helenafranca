87e426c89050041fb8ee54698553d3f2135ea87d
ricardo.franca86@gmail.com <ricardo.franca86@gmail.com@042fa983-7a08-99a7-bb0c-5fe388aa26e1>
2013-08-21 21:03:41 +0000

    - Conclusão de manter SlideShow.

--- trunk/helenaFranca/src/br/com/helenafranca/view/SlideShowMB.java
+++ trunk/helenaFranca/src/br/com/helenafranca/view/SlideShowMB.java
@@ -1,23 +1,12 @@
 package br.com.helenafranca.view;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.OutputStream;
 import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.List;
+import javax.faces.application.FacesMessage;
 import javax.faces.context.FacesContext;
-import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-
-import org.richfaces.event.UploadEvent;
-import org.richfaces.model.UploadItem;
-
-
 import br.com.helenafranca.controller.SlideShowFacade;
 import br.com.helenafranca.controller.SlideShowFacadeImpl;
 import br.com.helenafranca.controller.QuadroFacade;
@@ -63,40 +52,85 @@ public class SlideShowMB implements Serializable {
         rp.sendRedirect(rq.getContextPath() + "/pages/artista/manterSlideShow.jsf");
 		return "cadastraSucesso";
 	}
-
-	/*
-	public String updateCadastro() throws IOException {
-		QuadroFacade quadroService = new QuadroFacadeImpl();
-		
-		
-		if(getImagePath()!=null)
-		{
-			String nome = this.getCadastroQuadro().getFoto();
-			
-			if(nome.equals(null))
-			{
-				nome = "http://localhost:8081/imagensHelenaFranca/imgcontroleDeErro.jpg";
-			}
-			
-			nome = nome.substring(21);
-			nome = "C:/Program Files/Apache Software Foundation/Tomcat 6.0/webapps" + nome;
-					
-			File f = new File(nome);  
-			f.delete();
-			
-			this.getCadastroQuadro().setQuadroImagem(getImagePath());
-			
-		}
-		
-		quadroService.atualiza(this.getCadastroQuadro());
-		this.imagePath = null;
+	
+	public void escolheAlterarQuadro(Long indice) throws IOException
+	{
 		HttpSession session = (HttpSession) FacesContext.getCurrentInstance().getExternalContext().getSession(false);
-		session.setAttribute("quadroAtual", this.getCadastroQuadro());
-		this.quadro = new Quadro();
-
-		return "atualizaSucesso";
+		session.setAttribute("indiceDoSlideShow", indice);
+		
+		HttpServletResponse rp = (HttpServletResponse) FacesContext.getCurrentInstance().getExternalContext().getResponse();
+		HttpServletRequest rq = (HttpServletRequest) FacesContext.getCurrentInstance().getExternalContext().getRequest();
+		rp.sendRedirect(rq.getContextPath() + "/pages/artista/consultaQuadrosSlideShow.jsf");
 	}
-	*/
+	
+	public void escolheCadastrarQuadro(Long indice) throws IOException
+	{		
+		SlideShowFacade slideShowService = new SlideShowFacadeImpl();
+
+		HttpServletResponse rp = (HttpServletResponse) FacesContext.getCurrentInstance().getExternalContext().getResponse();
+		HttpServletRequest rq = (HttpServletRequest) FacesContext.getCurrentInstance().getExternalContext().getRequest();
+
+		FacesContext context = FacesContext.getCurrentInstance();
+		
+		FacesMessage facesMessage = new FacesMessage();
+		
+		if(slideShowService.procuraById(indice)==null)
+		{
+			rp.sendRedirect(rq.getContextPath() + "/pages/artista/cadastraQuadrosSlideShow.jsf");			
+		}else
+		 {
+			facesMessage.setSummary("Já existe uma quadro cadastrado.");
+			context.addMessage(null, facesMessage);
+			
+			rp.sendRedirect(rq.getContextPath() + "/pages/artista/manterSlideShow.jsf");
+		 }				
+	}
+	
+public void cadastraQuadroDoSlideShow() throws IOException {
+		
+
+		HttpSession session = (HttpSession) FacesContext.getCurrentInstance().getExternalContext().getSession(false);
+		HttpServletResponse response = (HttpServletResponse) FacesContext.getCurrentInstance().getExternalContext().getResponse();
+		HttpServletRequest request = (HttpServletRequest) FacesContext.getCurrentInstance().getExternalContext().getRequest();
+				
+		SlideShowFacade slideShowService = new SlideShowFacadeImpl();	
+		
+		QuadroFacade quadroService = new QuadroFacadeImpl();
+		Quadro quadro = quadroService.procuraById(codigoQuadro);
+		Artista artista = (Artista)session.getAttribute("artistaLogado");
+		
+		this.slideShow.setQuadro(quadro);		
+		this.slideShow.setArtista(artista);
+		
+		slideShowService.salva(this.slideShow);
+		
+		this.slideShow = new SlideShow();
+
+
+		response.sendRedirect(request.getContextPath() + "/pages/artista/manterSlideShow.jsf");
+	}
+	
+	public void alterarQuadroDoSlideShow() throws IOException {
+		
+		HttpSession session = (HttpSession) FacesContext.getCurrentInstance().getExternalContext().getSession(false);
+		HttpServletResponse response = (HttpServletResponse) FacesContext.getCurrentInstance().getExternalContext().getResponse();
+		HttpServletRequest request = (HttpServletRequest) FacesContext.getCurrentInstance().getExternalContext().getRequest();
+				
+		SlideShowFacade slideShowService = new SlideShowFacadeImpl();				
+		long indice = (Long) session.getAttribute("indiceDoSlideShow");		
+		this.slideShow = slideShowService.procuraById(indice);
+		
+		QuadroFacade quadroService = new QuadroFacadeImpl();
+		Quadro quadro = quadroService.procuraById(codigoQuadro);		
+		this.slideShow.setQuadro(quadro);		
+		
+		slideShowService.atualiza(this.slideShow);
+		this.slideShow = new SlideShow();
+
+
+		response.sendRedirect(request.getContextPath() + "/pages/artista/manterSlideShow.jsf");
+	}
+	
 	
 	public String procuraById() throws IOException
 	{	
